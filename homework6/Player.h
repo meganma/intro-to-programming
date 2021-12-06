c04386cddc93c901366877497e23011001a6e0fc
meganma <meganmasf@hotmail.com>
2021-12-05 16:15:53 -0800

    Update Player.h

--- homework6/Player.h
+++ homework6/Player.h
@@ -1,11 +1,13 @@
 /*
-    PIC 10A Homework 6, Player.cpp
-    Implementation file
+    PIC 10A Homework 6, Player.h
+    header file
     Author: Megan Ma
     Date: 11/17/2021
  
  */
-#include “Player.h”
+
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
 
 #include<iostream>
 #include<ctime>
@@ -13,89 +15,35 @@
 #include<string>
 using namespace std;
 
-
-/************************************************
-  methods implementation
- ***********************************************/
-
-Player::Player()
+class Player
 {
-    name = "MyPlayer";
-    health = 0;
-    n_battle = 0;
-    n_win = 0;
-}
+    public:
+        /**
+         Constructs a Player with name "MyPlayer" and health 0, n_battle 0, and n_win 0.
+         */
+        Player();
+        /**
+         Constructs a Player with name "myname" and health mystamina, n_battle mybattles, and n_win 0.
+         */
+        Player(string myname, int mystamina, int mybattles);
+        /**
+          Mimics the process of having 1 battle.
+          @param enemy the object the player is playing against
+          @return true if the player wins
+          */
+        bool battle(Player& enemy);
+        /**
+         Mimics the process of having one game (multiple battles)
+         @param enemy the object the player is playing against
+         @return true if the player wins the game (player wins more battles than enemy)
+         */
+        bool game(Player& enemy);
+    //private variables
+    private:
+        string name;
+        int health;
+        int n_battle;
+        int n_win;
+};
 
-Player::Player(string myname, int mystamina, int mybattles)
-{
-    name = myname;
-    health = mystamina;
-    n_battle = mybattles;
-    n_win = 0;
-}
-
-bool Player::battle(Player& enemy){
-    //declare variables to hold the number of points the player/enemy uses this battle
-    int userhealthpoints, enemyhealthpoints;
-    cout << "There are " << n_battle << " more battles left." << endl;
-    cout << "You have " << health << " health points left." << endl;
-    cout << "How many health points do you want to use? ";
-    //store user input to the var userhealthpoints
-    cin >> userhealthpoints;
-    //don't allow the user to use more points than their current health
-    if(userhealthpoints > health){
-        userhealthpoints = health;
-    }
-    cout << name << " choose to use " << userhealthpoints << " health points." << endl;
-    
-    if(n_battle == 1){
-        //use all of the enemy's points on the last battle
-        enemyhealthpoints = enemy.health;
-    }else{
-        //if it isn't the last battle, the enemy uses a random int of health points in the range from 1 to their total health points
-        enemyhealthpoints = rand() % enemy.health + 1;
-    }
-    cout << enemy.name << " choose to use " << enemyhealthpoints << " health points." << endl;
-    
-    //update new total health points
-    health = health - userhealthpoints;
-    enemy.health = enemy.health - enemyhealthpoints;
-    
-    if(enemyhealthpoints >= userhealthpoints){
-        //return false if enemy wins the battle
-        enemy.n_win = enemy.n_win + 1; //increase enemy win count
-        cout << enemy.name << " wins this battle!\n" ;
-        cout << "****************************************************\n" ;
-        return false;
-    }else{
-        //return true if user wins the battle
-        n_win = n_win + 1; //increase user win count
-        cout << name << " wins this battle!\n";
-        cout << "****************************************************\n" ;
-        return true;
-    }
-}
-
-bool Player::game(Player& enemy)
-{
-    bool count = true;
-    //repeat battles until the number of battles is 0
-    while(count == true){
-        battle(enemy); //user battle the enemy
-        n_battle = n_battle - 1; //update number of battles
-        if(n_battle == 0){
-            count = false;
-        }
-    }
-    
-    if(n_win > enemy.n_win){
-        // return true if player wins the game
-        cout << "The final winner is " << name << "."<< endl;
-        return true;
-    }else{
-        //return false if enemy wins the game
-        cout << "The final winner is " << enemy.name << "." << endl;
-        return false;
-    }
-}
-
+#endif
